5824a15fbfcdabb282415d82c6a337696feeafd9
Matthew Brown <matt@maxwellmri.com>
2019-04-28 17:42:44 +0100

    Move init inline

--- scripts/apply_terraform.sh
+++ scripts/apply_terraform.sh
@@ -6,6 +6,7 @@ VAR_FILE="$(pwd)/secrets/${ENVIRONMENT:-dev}.json"
 echo $VAR_FILE
 
 pushd infra
+terraform init
 terraform apply -var-file="${VAR_FILE}"
 terraform output --json > outputs.json
 popd
