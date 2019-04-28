75162eb90a6e6203474a007682fb327e565516e1
Matthew Brown <matt@maxwellmri.com>
2019-04-28 15:27:58 +0100

    Don't install gitcryopt

--- scripts/install_yarn.sh
+++ scripts/install_yarn.sh
@@ -4,4 +4,4 @@ apt-get install apt-transport-https
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt-get update -y
-apt-get install -y yarn git-crypt
+apt-get install -y yarn
