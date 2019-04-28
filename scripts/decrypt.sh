1168f37f08c43c3682a670253ed6a5e834494284
Matthew Brown <me@matthewbrown.io>
2019-04-28 18:03:12 +0100

    cleanup build

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 set -ev
-echo "$CRYPT_KEY" | base64 -d > crypt.key
-ls -lah
-git-crypt unlock crypt.key
+echo "$CRYPT_KEY" | base64 -d > crypt.key && git-crypt unlock crypt.key
 rm -rf crypt.key
--- scripts/deploy_sls.sh
+++ scripts/deploy_sls.sh
@@ -3,5 +3,6 @@
 set -ve
 
 pushd backend
+yarn --frozen-lockfile
 yarn deploy
 popd
