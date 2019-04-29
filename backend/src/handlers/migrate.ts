63883405f834f88898f93400aa812a882181df46
Matthew Brown <me@matthewbrown.io>
2019-04-29 07:10:11 +0100

    Don't log connection info

--- backend/src/handlers/migrate.ts
+++ backend/src/handlers/migrate.ts
@@ -1,9 +1,8 @@
 import { Handler } from "aws-lambda";
-import { db, config } from "../db";
+import { db } from "../db";
 import { join } from "path";
 
 export const handler: Handler = async () => {
-  console.log(config);
   await db.migrate.latest({
     directory: join(process.cwd(), "migrations")
   });
