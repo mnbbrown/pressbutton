e7dc9a1492d3db11df06c26796e408c18dfb0cbf
Matthew Brown <me@matthewbrown.io>
2019-04-29 07:47:54 +0100

    Move to using remote state

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = "eu-west-2"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "pressbutton-terraform-state"
+    key = "terraform.tfstate"
+    region = "eu-west-2"
+    encrypt = true
+  }
+}
