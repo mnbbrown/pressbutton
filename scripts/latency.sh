04a721055cd3edb93e2ad9654ee364f56bcd363e
Matthew Brown <me@matthewbrown.io>
2019-05-02 10:42:39 +0100

    Fix latency to warm lambda first

--- scripts/latency.sh
+++ scripts/latency.sh
@@ -1,4 +1,9 @@
-wrk2 -t2 -c50 -d10s -R10 --latency https://${ENVIRONMENT:-dev}-api.pushbutton.dev/status > 10
-wrk2 -t2 -c50 -d10s -R50 --latency https://${ENVIRONMENT:-dev}-api.pushbutton.dev/status > 50
-wrk2 -t2 -c50 -d10s -R100 --latency https://${ENVIRONMENT:-dev}-api.pushbutton.dev/status > 100
+
+set -ev
+
+URL=https://${ENVIRONMENT:-dev}-api.pushbutton.dev/status
+curl -v $URL
+wrk2 -t2 -c50 -d10s -R10 --latency $URL > 10
+wrk2 -t2 -c50 -d10s -R50 --latency $URL > 50
+wrk2 -t2 -c50 -d10s -R100 --latency $URL > 100
 cat 10 50 100 | wrk2img latency.png
