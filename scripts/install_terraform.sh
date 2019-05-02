e78066c05c2802fc9d601ab47fed00465a0afc78
Matthew Brown <me@matthewbrown.io>
2019-05-02 10:45:14 +0100

    Cleanup terraform

--- scripts/install_terraform.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-TERRAFORM_VERSION=0.11.13
-
-apt-get install unzip
-wget --quiet https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-install terraform /usr/local/bin/
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
